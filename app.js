23ee5f84536072ef35c42a13311e186fa0216097
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-20 11:42:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const {client , connectToCluster} = require('./mongodbconnection');
+
+
+app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+const port = process.env.port || 5001;
+
+const connectToMongoCluster = async ()=> {
+
+    try {
+    await connectToCluster();
+    console.log("connected to mongo cluster");
+
+    } catch(e) {
+        console.log(e.message);
+    }
+    
+}
+
+connectToMongoCluster();
+
+
+app.get("/" , (req , res)=> {
+    console.log("home route");
+    res.send("hello");
+})
+
+
+
+app.listen(port , ()=> {
+    console.log(`connected to http://localhost:5001`);
+})
+
+
---
+++ mongodbconnection.js
@@ -0,0 +1,30 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+
+const url = process.env.MONGO_DB_URL;
+
+
+const client = new MongoClient(url , {
+    serverApi: ServerApiVersion.v1
+})
+
+const connectToCluster = async ()=> {
+
+    try {
+    await client.connect();
+    console.log("successfully connected to db");
+    // console.log(promise);
+    }
+    catch(e) {
+        console.log(e.message);
+    }
+}
+
+// connectToCluster();
+
+
+module.exports = {client , connectToCluster};
+
