6131f9b7c82dcc22d34e42b3c8d188494aceee49
Ganesh7386 <puttaganesh7386@gmail.com>
2024-06-20 12:05:22 +0530

    first commit

--- app.js
+++ app.js
@@ -33,6 +33,15 @@ app.get("/" , (req , res)=> {
 })
 
 
+app.get("/sales" , async (req , res)=> {
+    const company_database = await client.db('transport');
+    const all_data_collection = await company_database.collection('all_data');
+    const got_data = await all_data_collection.findOne({end_year : 2027});
+    console.log(got_data);
+
+    res.json(got_data);
+})
+
 
 app.listen(port , ()=> {
     console.log(`connected to http://localhost:5001`);
